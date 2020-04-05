d79b7962131db882971a4863bd4ea1ce2e9d2923
Tanishq Abraham <tmabraham@ucdavis.edu>
2020-04-05 01:51:20 +0000

    Here's what I have so far. ParallelLoader is not returning XLA tensors. I will write a test script for using ParallelLoader with TfmDL.

--- fastai_v2/tpu_distributed_fastai2.py
+++ fastai_v2/tpu_distributed_fastai2.py
@@ -10,41 +10,50 @@ import torch_xla.distributed.xla_multiprocessing as xmp
 import torch
 
 import fastai2
-from fastai import *
-from fastai.core import *
-from fastai.torch_core import *
-from fastai.vision import *
-from fastai.basic_train import *
+from fastai2.callback.all import *
+from fastai2.vision.all import *
+from fastai2.distributed import *
+
 
 def len_parallelloader(self): return len(self._loader._loader)
 pl.PerDeviceLoader.__len__ = len_parallelloader
 
+@patch
+def set_epoch(self:pl.PerDeviceLoader,epoch): 
+       self._loader._loader.set_epoch(epoch)
 
 
 
 
 class TPUDistributed(Callback):
-    def __init__(self, learn:Learner):
+    def __init__(self):
         self.device = xm.xla_device()
 
     def _wrap_dl(self, dl):
 
-        if isinstance(dl, pl.ParallelLoader):
+        if isinstance(dl, pl.PerDeviceLoader):
             return dl
         else:
-            return pl.ParallelLoader(DistributedDL.from_dl(dl, xm.get_ordinal(), xm.xrt_world_size()), [self.device]).per_device_loader(self.device)
+            dl.fake_l.num_workers=0 # for some reason, needed for it to work. Investigate further
+            distributed_dl = DistributedDL.from_dl(dl, xm.get_ordinal(), xm.xrt_world_size())
+            distributed_dl.epoch=0
+            return pl.ParallelLoader(distributed_dl, [self.device]).per_device_loader(self.device)
 
     def begin_fit(self):
+        xm.master_print('begin fit')
         self.learn.model = self.learn.model.to(self.device)
-        self.learn.opt.lr = self.learn.opt.lr*xm.xrt_world_size()
+        for h in self.opt.hypers: h['lr'] *= xm.xrt_world_size()
         self.old_dls = list(self.dls)
+        print('wrapping dls')
         self.learn.dls.loaders = [self._wrap_dl(dl) for dl in self.dls]
-
+        for dl in self.dls: dl.set_epoch(self.epoch)
 
     def begin_epoch(self):
         for dl in self.dls: dl.set_epoch(self.epoch)
 
-    def begin_train(self):    self.learn.dl = self._wrap_dl(self.learn.dl)
+    def begin_train(self):    
+        self.learn.dl = self._wrap_dl(self.learn.dl)
+        #print(next(iter(self.learn.dl))[0].type()) <-- returns a regular PyTorch Tensor
 
     def after_backward(self):
         xm.optimizer_step(self.learn.opt)
@@ -57,13 +66,11 @@ class TPUDistributed(Callback):
     def after_fit(self):
         self.learn.dls.loaders = self.old_dls
 
+@patch
+def to_tpu_distributed(self:Learner):
+    self.add_cbs([TPUDistributed()])
+    return self
 
-def _to_tpu_distributed(learn:Learner) -> Learner:
-    learn.callback_fns.append(TPUDistributed)
-    return learn
-
-
-Learner.to_tpu_distributed = _to_tpu_distributed
 
 
 def filelist2df(path):
@@ -75,19 +82,20 @@ path = untar_data(URLs.FOOD)
 train_path = path/'train.txt'
 test_path = path/'test.txt'
 
-def train_loop():
+def train_loop(index):
+    print('index: ',index)
     train_df = filelist2df(train_path)
     test_df = filelist2df(test_path)
     food = DataBlock(blocks=(ImageBlock, CategoryBlock),
                  get_x = ColReader(1,pref=path/'images'),
                  splitter = RandomSplitter(),
                  get_y = ColReader(cols=0),
-                 item_tfms=Resize(224),
-                 batch_tfms=aug_transforms(flip_vert=True, max_lighting=0.1, max_zoom=1.05, max_warp=0.)
+                 item_tfms=Resize(224)
+#                 batch_tfms=aug_transforms(flip_vert=True, max_lighting=0.1, max_zoom=1.05, max_warp=0.) <-- ignore batch (on-TPU) tfms for now
                  )
     dls = food.dataloaders(train_df.values,bs=64)
-    learn = cnn_learner(dls, models.resnet152, metrics=accuracy).to_tpu_distributed()
+    learn = cnn_learner(dls, resnet152, metrics=accuracy).to_tpu_distributed()
     learn.fit(3)
 
 if __name__ == "__main__":
-  xmp.spawn(train_loop,args=())
+  xmp.spawn(train_loop,nprocs=8,args=())
